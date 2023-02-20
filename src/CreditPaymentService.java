afe033b27df4922cbf132cf9c0c3d1cffae65771
Tatiana Rem <tatiana13d@yandex.ru>
2023-02-20 15:57:56 +0600

    Changes for the second branch

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,6 @@
 public class CreditPaymentService {
     public double calculate(double creditDuration, double creditSum, double yearPercent) {
-        double monthlyPercent = yearPercent / 12 / 100;
+        double monthlyPercent = yearPercent / creditDuration / 100;
         double annuitet = monthlyPercent * Math.pow(1 + monthlyPercent, creditDuration) / (Math.pow(1 + monthlyPercent, creditDuration) - 1);
         double monthlyPayment = creditSum * annuitet;
         return monthlyPayment;
